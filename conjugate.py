8a11bba310c67fb6f51a87c5e5bfcf4ba9dd7be0
duanwenbo <bobbyduanwenbo@live.com>
2021-12-04 16:31:41 +0000

    support min and max

--- conjugate.py
+++ conjugate.py
@@ -1,29 +1,35 @@
-from operator import pos
 import sympy as sp  # tools for matrix, calculus operation
+import os
 
 ################ edit aera ########################
 # TODO: mattrix format of input obj func
 """set the objective function there"""
 # 1. declare variables
-x_1, x_2, x_3 = sp.symbols('x_1 x_2 x_3') 
+x_1, x_2 = sp.symbols('x_1 x_2') 
 # 2. initialize obj func
-f = (x_1 -1)**2 + (x_2 -2)**2 +(x_3 -3)**2
+# f = (x_1 -1)**2 + (x_2 -2)**2 +(x_3 -3)**2
+f = -x_1**2 + 4*x_1 + 2*x_1*x_2 - 2*x_2**2
 # 3. declare init position
-starting_position = [(x_1,0.5), (x_2,0.5), (x_3, 0.5)]  # standard format of position
+starting_position = [(x_1,0.), (x_2,0.)]  # standard format of position
+
 #####################################################
 alpha = sp.symbols('alpha')  # declare step lenghth var
+gs_test = -16*alpha**2 + 16*alpha  # unimodal   
 
 EPSILON = 0.005  # tolerance
 GOLDEN_RATIO = 0.618
 
 
+class Objective_function:
+    def __init__(self) -> None:
+        pass
 
 def _update_position(position_vector, current_position, step_length = alpha):
     assert len(starting_position) == sp.shape(position_vector)[1], "check beta value!"
     # convert from standart format into matrix
     current_position = sp.Matrix([[i[1] for i in current_position]])
-    # θ' = θ - α*▽f(θ)
-    next_position = current_position - step_length*position_vector
+    # θ' = θ + α*▽f(θ)
+    next_position = current_position + step_length*position_vector
     # convert from matrix into standard format
     value = [i for i in next_position]
     result = [(starting_position[i][0], (value[i])) for i in range(len(value))]
@@ -66,63 +72,59 @@ def _record(position, direction_vector, beta, step_length, initial = False):
         f.write("where direction updating parameter beta: {}\n".format(beta))
         f.write("step length parameter alpha:{}\n\n".format(step_length))
 
+def detect_previous_doc():
+    if os.path.exists("log.txt"):
+        os.remove("log.txt")
+    if os.path.exists("solution.txt"):
+        os.remove("solution.txt")
 
-
-def golden_search(func):
+def  golden_search(func):
     # find the optimal step length
     a,b = -99.,99. 
-    opt_point = (b+a) / 2
-    gradient = func.diff(alpha).subs([(alpha,opt_point)])
-    while abs(gradient) > EPSILON:
+    difference = abs(a-b)
+    while difference > EPSILON:
         distance = GOLDEN_RATIO * (b-a)
         m,n = a+distance, b-distance
         fm, fn = func.subs(alpha,m), func.subs(alpha,n)
-        if fm > fn:
+        if fm < fn:  # > minimisation < maximisation
             b = m
-        elif fm < fn:
-            a = n
         else:
-            break
-        opt_point = (b+a) / 2
+            a = n
+        difference = abs(a-b) 
+    opt_point = (b+a) / 2
     return opt_point
 
 def conjugate_gradient():
     # first round cycle
     current_position = starting_position
     gradient= _grad(f, current_position)
-    position_vector =  - gradient  # when j=1
-    next_position_var = _update_position(position_vector, current_position)
-    next_positon_func = f.subs(next_position_var)
-    step_length = golden_search(next_positon_func)
+    position_vector =  gradient  # when j=1
+    next_position_var = _update_position( position_vector, current_position)
+    obj_func = f.subs(next_position_var)
+    step_length = abs(golden_search(obj_func))
     next_position = _update_position(position_vector, current_position, step_length)
 
-    _record(current_position, position_vector, 0, step_length, True)
+    # _record(current_position, position_vector, 0, step_length, True)
     current_position = next_position
-
 
     stop = False
     while not stop:
         new_gradient = _grad(f, current_position)
         beta = _HS_form(new_gradient, gradient)  # searching beta
-        new_position_vector = - new_gradient + beta * position_vector
-        next_position_var = _update_position(new_position_vector, current_position)
-        next_positon_func = f.subs(next_position_var)
-        step_length = golden_search(next_positon_func)  # searching alpha
-        next_position = _update_position(position_vector, current_position, step_length)
+        new_position_vector =  new_gradient - beta * position_vector
+        next_position_var = _update_position( new_position_vector, current_position)
+        obj_func = f.subs(next_position_var)
+        step_length = abs(golden_search(obj_func))  # searching alpha
+        next_position = _update_position(new_position_vector, current_position, step_length)
 
-        _record(current_position, new_position_vector, beta, step_length)
-
+        # _record(current_position, new_position_vector, beta, step_length)
         # update parameters
         current_position = next_position
         gradient = new_gradient
         position_vector = new_position_vector
-
-        print(current_position)
-        print(beta)       
-        print("\n")
+        print(step_length)
         if _modulus(position_vector) < EPSILON:
             stop = True
-            print(current_position)
 
 def steepest_gradient():
     current_position = starting_position
@@ -133,13 +135,18 @@ def steepest_gradient():
             stop = True
             print(current_position)
         else:
-            next_position_var = _update_position (-position_vector, current_position)
-            next_position_func = f.subs(next_position_var)
-            derivative = sp.diff(next_position_func, alpha)
-            step_len = sp.solve(derivative, alpha)[0]
-            next_position = _update_position(-position_vector, current_position, step_len)
+            next_position_var = _update_position ( position_vector, current_position)
+            obj_func = f.subs(next_position_var)
+            derivative = sp.diff(obj_func, alpha)
+            step_len = abs(sp.solve(derivative, alpha)[0])
+            next_position = _update_position( position_vector, current_position, step_len)  # - for minimization
             current_position = next_position
        
 
 if __name__ == "__main__":
-   conjugate_gradient()
+    detect_previous_doc()
+    # print("####################SG#############")
+    # steepest_gradient()  
+    print("####################CONJU#############")
+    conjugate_gradient()
+ 
